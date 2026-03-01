fc25bc120c86f08934d3ace7542ad5f835be41c1
Vignesh <vignesh24cs@rnsit.ac.in>
2026-03-01 09:04:13 +0530

    arrays

--- arra.java
+++ arra.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class arra {
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
      
-    int[]
+    int[][] arr = new int[3][3];
+
+    for(int i=0;i<arr.length;i++)
+    {
+        for(int j=0;j<arr[i].length;j++)
+        {
+            arr[i][j]=sc.nextInt();
+        }
+    }
+    for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr[i].length;j++){
+            System.out.print(arr[i][j] +" ");
+        }
+        System.out.println();
+    }
 
     }
     
